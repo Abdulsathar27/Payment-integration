52e82bfac15f2326935bd92dbd364ac675682423
Abdulsathar27 <abdulsatharnechithodi@gmail.com>
2026-05-16 15:24:03 +0530

    update in payment screen

--- lib/view/screens/payment_screen.dart
+++ lib/view/screens/payment_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:paymentin/core/constant/app_colors.dart';
 import 'package:paymentin/models/transaction_model.dart';
-import 'package:paymentin/view/widgets/payment_card.dart';
-import 'package:paymentin/view/widgets/status_card.dart';
-import 'package:paymentin/view/widgets/timer_widget.dart';
-import 'package:paymentin/view_model/payment_provider.dart';
 import 'package:provider/provider.dart';
+import '../../view_model/payment_provider.dart';
+import '../widgets/payment_card.dart';
+import '../widgets/status_card.dart';
+import '../widgets/timer_widget.dart';
 
 class PaymentScreen extends StatelessWidget {
   const PaymentScreen({super.key});
@@ -15,7 +15,7 @@ class PaymentScreen extends StatelessWidget {
     final provider = Provider.of<PaymentProvider>(context);
 
     final transaction = TransactionModel(
-      merchantName: "Netflix Permium",
+      merchantName: "Netflix Premium",
       amount: 799,
       cardNumber: "1234567812345678",
     );
@@ -24,22 +24,60 @@ class PaymentScreen extends StatelessWidget {
       backgroundColor: AppColors.background,
       appBar: AppBar(
         backgroundColor: AppColors.background,
+        elevation: 0,
+        centerTitle: true,
         title: const Text(
           "Secure Transaction",
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            PaymentCard(transaction: transaction),
-            const SizedBox(height: 20),
-            TimerWidget(seconds: provider.remainingSeconds),
+            const Text(
+              "Secure Payment Gateway",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 8),
+
+            const Text(
+              "Your transaction is encrypted and secure",
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 16,
+              ),
+            ),
+
             const SizedBox(height: 30),
-            StatusCard(status: provider.paymentStatus),
+
+            PaymentCard(
+              transaction: transaction,
+            ),
+
+            const SizedBox(height: 25),
+
+            TimerWidget(
+              seconds: provider.remainingSeconds,
+            ),
+
+            const SizedBox(height: 30),
+
+            StatusCard(
+              status: provider.paymentStatus,
+            ),
+
             const Spacer(),
+
             if (provider.paymentStatus == PaymentStatus.failed ||
                 provider.paymentStatus == PaymentStatus.timeout)
               Padding(
@@ -52,57 +90,78 @@ class PaymentScreen extends StatelessWidget {
                       provider.retryTransaction();
                     },
                     style: OutlinedButton.styleFrom(
-                      side: const BorderSide(color: Colors.white),
+                      side: const BorderSide(
+                        color: Colors.white,
+                      ),
                     ),
                     child: const Text(
-                      "Retry Transactions",
-                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      "Retry Transaction",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
                     ),
                   ),
                 ),
               ),
+
             SizedBox(
               width: double.infinity,
               height: 60,
               child: ElevatedButton(
                 onPressed:
                     provider.paymentStatus == PaymentStatus.loading ||
-                        provider.paymentStatus == PaymentStatus.success ||
-                        provider.paymentStatus == PaymentStatus.timeout
-                    ? null
-                    : () {
-                        provider.confirmPayment();
-                      },
+                            provider.paymentStatus ==
+                                PaymentStatus.success ||
+                            provider.paymentStatus ==
+                                PaymentStatus.timeout
+                        ? null
+                        : () {
+                            provider.confirmPayment();
+                          },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: AppColors.primary,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
                 ),
-                child: provider.paymentStatus == PaymentStatus.loading
-                    ? const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            height: 20,
-                            width: 20,
-                            child: CircularProgressIndicator(
-                              strokeWidth: 2,
-                              color: Colors.white,
+                child:
+                    provider.paymentStatus == PaymentStatus.loading
+                        ? const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: 20,
+                              width: 20,
+                              child: CircularProgressIndicator(
+                                strokeWidth: 2,
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
 
-                          SizedBox(width: 15),
+                            SizedBox(width: 15),
 
-                          Text(
-                            "Processing...",
-                            style: TextStyle(color: Colors.white, fontSize: 18),
+                            Text(
+                              "Processing...",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        )
+                        : const Text(
+                          "Confirm Payment",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
                           ),
-                        ],
-                      )
-                    : const Text(
-                        "Confirm Payment",
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
+                        ),
               ),
             ),
+
+            const SizedBox(height: 20),
           ],
         ),
       ),
